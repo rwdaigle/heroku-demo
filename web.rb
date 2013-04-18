196a07447ffc6b196d462e289692498b8d12b373
Mark McGranaghan <mmcgrana@gmail.com>
2013-04-18 07:53:26 -0700

    location endpoint

--- web.rb
+++ web.rb
@@ -1,6 +1,15 @@
 require "sinatra"
+require "rest-client"
+require "json"
 
 get "/" do
   content_type "text/html"
   File.read("public/index.html")
+end
+
+get "/location" do
+  ip = RestClient.get("http://ipecho.net/plain")
+  location = RestClient.get("http://freegeoip.net/json/#{ip}")
+  content_type "application/json"
+  location
 end
