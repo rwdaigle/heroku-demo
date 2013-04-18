fefc348ccb0b5d1a4f1c537c4e8824637f9514b8
Mark McGranaghan <mmcgrana@gmail.com>
2013-04-18 08:10:52 -0700

    really working

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,17 +1,18 @@
 function initMap() {
-  var location = {latitude: 37.555, longitude: -122.2687}
-  var latLng = new google.maps.LatLng(location.latitude, location.longitude);
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: latLng,
-    zoom: 4,
-    disableDefaultUI: true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var marker = new google.maps.Marker({
-      position: latLng,
-      icon: "https://maps.google.com/mapfiles/ms/icons/purple-dot.png",
-      animation: google.maps.Animation.DROP,
-      map: map
+  $.getJSON("/location", function(location) {
+    var latLng = new google.maps.LatLng(location.latitude, location.longitude);
+    var map = new google.maps.Map(document.getElementById("map"), {
+      center: latLng,
+      zoom: 4,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var marker = new google.maps.Marker({
+        position: latLng,
+        icon: "https://maps.google.com/mapfiles/ms/icons/purple-dot.png",
+        animation: google.maps.Animation.DROP,
+        map: map
+    });
   });
 }
 
