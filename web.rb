5182bba2c9aa4bf5ca68b4d3ed458d4ee70adf13
Ryan Daigle <ryan.daigle@gmail.com>
2013-06-13 22:11:40 -0400

    Change IP lookup providers

--- web.rb
+++ web.rb
@@ -8,7 +8,8 @@ get "/" do
 end
 
 get "/location" do
-  ip = RestClient.get("http://ipecho.net/plain")
+  # ip = RestClient.get("http://ipecho.net/plain")
+  ip = RestClient.get("http://bot.whatismyipaddress.com/")
   location = RestClient.get("http://freegeoip.net/json/#{ip}")
   content_type "application/json"
   location
